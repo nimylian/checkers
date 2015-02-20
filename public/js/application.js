f86b41becc1fd1eda2d462e514f0423275eb931a
John Schulz <johnschulz75@gmail.com>
2015-02-20 09:13:54 -0600

    Changed formatting of pieces to universal piece class with name attribute defining the color

--- public/js/application.js
+++ public/js/application.js
@@ -1,23 +1,21 @@
-turn = "one"
-
+turn = "red"
 $(document).ready(function() {
-
 	var redTile = "<div class='red_tile'></div>"
 	var blackTile = "<div class='black_tile'></div>"
-	var redPiece = "<button class='red_piece' type='red'></button>"
-	var blackPiece = "<button class='black_piece'></button>"
+	var redPiece = "<button class='piece' name='red'></button>"
+	var blackPiece = "<button class='piece' name='black'></button>"
 
-	var containerWidth = $('.container').width()
-	$('.container').css("height", containerWidth)
-	$('.container').css("width", containerWidth)
+	var containerWidth = $('.table-top').width()
+	$('.table-top').css("height", containerWidth)
+	$('.table-top').css("width", containerWidth)
 	for (var i=0; i<4; i++){
 		for (var x=0; x<4; x++) {
-		  $('.container').append(redTile)
-		  $('.container').append(blackTile)
+		  $('.table-top').append(redTile)
+		  $('.table-top').append(blackTile)
 		};
 		for (var x=0; x<4; x++) {
-		  $('.container').append(blackTile)
-		  $('.container').append(redTile)
+		  $('.table-top').append(blackTile)
+		  $('.table-top').append(redTile)
 		};
 	}
 	for (var i=0; i<12; i++){
@@ -27,24 +25,21 @@ $(document).ready(function() {
 		$('.black_tile').eq(i).append(redPiece);
 	}
 
-	$(document).on( "click", ".red_piece[type='red]", function(event) {
+	$(document).on( "click", ".piece", function(event) {
 
-		if ($(event.target).attr('class') === 'red_piece'){
-			console.log("red piece clicked");
+		if ($(event.target).attr('class') === 'piece'){
+			console.log("piece clicked");
   		$(event.target).remove();
 		}
 	});
 
-
-
 // Placing Red Pieces - can not be placed where one already exists
 	$(document).on( "click", '.black_tile', function(event) {
 		if ($(event.target).attr('class') === 'black_tile'){
-			if ($(event.target).children().attr('class') === 'red_piece' || $(event.target).children().attr('class') === 'black_piece')
+			if ($(event.target).children().attr('class') === 'piece' || $(event.target).children().attr('class') === 'black_piece')
 		{} else {
 			console.log("success");
   		$(event.target).append(redPiece);
 		}}
 	});
-
 });
